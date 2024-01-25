b63cbe3b4b1af4dd12815c6eb9f3ccaeeae6d0e0
git@github.com <savvy.liu@hotmail.com>
2024-01-24 23:31:02 -0500

    Fixed bug with exit spaces, and finished MVP, also fixed issue tag type, closes #8, closes #13)

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Solver.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Solver.java
@@ -1,10 +1,51 @@
 package ca.mcmaster.se2aa4.mazerunner;
-import java.time.Instant;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
+
+import java.util.ArrayList;
 
 public class Solver {
+    public ArrayList<String> output = new ArrayList<String>();
+    Maze toSolve;
+
+    enum directions {
+        RIGHT,
+        LEFT,
+        UP,
+        DOWN
+      }
+
+
     public Solver(Maze toSolve) { 
-        // Solve the maze
+        this.toSolve = toSolve;
+    }
+
+    public void solve(){
+        for (int i = 0; i < toSolve.xSize(); i ++){
+            toSolve.goRight();
+            recordMovement(directions.RIGHT);
+        }
+    }
+
+    public void recordMovement(directions d){
+        switch (d){
+            case RIGHT:
+                output.add("R");
+                break;
+            case LEFT:
+                output.add("L");
+                break;
+            case UP:
+                output.add("U");
+                break;
+            case DOWN:
+                output.add("D");
+                break;
+        }
+    }
+
+    public void result(){
+        for (int i = 0; i < output.size(); i++){
+            System.out.print(output.get(i));
+        }
+        System.out.println();
     }
 }
