403d531d563f0ac0ffbd82eee6bdfa32952311b6
git@github.com <savvy.liu@hotmail.com>
2024-01-24 23:25:34 -0500

    Fixed bug with exit spaces, and finished MVP, closes #1, closes #2)

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
@@ -1,13 +1,15 @@
 package ca.mcmaster.se2aa4.mazerunner;
-
-import java.util.ArrayList;
-
+import java.util.Arrays;
 public class Maze {
-    int x = 0;
-    int y = 0;
-    String[][] grid;
+    private int x = 0;
+    private int y = 0;
+    private String[][] grid;
+
     public Maze(int x, int y){
-        grid = new String[x][y];
+        grid = new String[y][x];
+        for (int i = 0; i < y; i++){
+            Arrays.fill(grid[i], " ");
+        }
     }
 
     public void goRight(){
@@ -17,6 +19,24 @@ public class Maze {
         x = 0;
         y++;
     }
+
+    public int xSize(){
+        return grid[0].length;
+    }
+
+    public int ySize(){
+        return grid.length;
+    }
+
+    public int enterance(){
+        for (int i = 0; i < y; i++){
+            if (grid[i][0] == null || grid[i][0].equals(" ")){
+                return i;
+            }
+        }
+        return 0;
+    }
+
     public void wall(Boolean t){
         if (t){
             grid[y][x] = "#";
@@ -26,11 +46,15 @@ public class Maze {
     }
 
     public void print(){
-        for (int i = 0; i < grid[0].length; i++){
-            for (int j = 0; j < grid.length; j++){
+        for (int i = 0; i < grid.length; i++){
+            for (int j = 0; j < grid[0].length; j++){
                 System.out.print(grid[i][j] + " ");
             }
             System.out.println();
         }
+    }
+
+    public void fix(){
+        
     }
 }
